2deddc78cee15c7e22aa603770306dec176a81b4
Korbinian Breu <korbinian.breu@payworksmobile.com>
2015-11-07 16:28:56 +0100

    fixed

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, send_from_directory
+from figo import FigoConnection, FigoSession
 import os
+import webbrowser
+
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
+
+@app.route('/account')
+def account():
+  session = FigoSession("ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ")
+  print session
+  securities = session.securities
+  #for security in session.get_securities():
+  #  print security
+  return 'alkjalkaj';
+
 
 @app.route('/<path:path>')
 def send_js(path):
